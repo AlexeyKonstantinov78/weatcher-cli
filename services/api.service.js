cde589774a8398ddfbf62ae2c5278979930fc88b
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-01-25 15:02:33 +0300

    получение данных через axios

--- services/api.service.js
+++ services/api.service.js
@@ -1,5 +1,6 @@
 import https from 'https';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+import axios from 'axios';
 
 const getWeather = async (city) => {
   const token = await getKeyValue(TOKEN_DICTIONARY.token);  
@@ -7,26 +8,36 @@ const getWeather = async (city) => {
     throw new Error('нет токена API, задайте через команду');
   }
 
-  const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-  url.searchParams.append('q', city);
-  url.searchParams.append('appid', token);
-  url.searchParams.append('lang', 'ru');
-  url.searchParams.append('units', 'metric');    
-
-  https.get(url, (response) => {
-    let res = '';
-    response.on('data', (chunk) => {
-      res += chunk;
-    });
-
-    response.on('end', () => {
-      console.log(res);
-    });
-
-    response.on('error', (err) => {
-      console.log(err.message);
-    });
+  const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+    params: {
+      q: city,
+      appid: token,
+      lang: 'ru',
+      units: 'metric'
+    }
   });
+
+  // const url = new URL('https://api.openweathermap.org/data/2.5/weather');
+  // url.searchParams.append('q', city);
+  // url.searchParams.append('appid', token);
+  // url.searchParams.append('lang', 'ru');
+  // url.searchParams.append('units', 'metric');    
+
+  // https.get(url, (response) => {
+  //   let res = '';
+  //   response.on('data', (chunk) => {
+  //     res += chunk;
+  //   });
+
+  //   response.on('end', () => {
+  //     console.log(res);
+  //   });
+
+  //   response.on('error', (err) => {
+  //     console.log(err.message);
+  //   });
+  // });
+  return data;
 };
 
 export { getWeather };
